3c8b50b6c221e761ba92fa16b46be863276f0292
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2019-07-04 15:06:01 +0300

    add routines to accept packets, and test pop protocol
    Main Module:
    
    server_loop and serve_peer routines
    accepting udp connection, reading data into buffer
    and tests pop protocol module.
    
    Builds/Scripts:
    
    thread/server/test-server.sh
    
    script that sends test packets to the server with netcat
    
    thread/server/makefile
    
    add <test> job: cleans, builds, runs server with valgrind
    mem leaks check at port 3000, and spawns test-server.sh
    script with 10 secs delay(in parallel)

--- thread/server/main.c
+++ thread/server/main.c
@@ -1,24 +1,94 @@
 #define _POSIX_C_SOURCE (200809L)
 #define _XOPEN_SOURCE (700)
-#include <stdio.h>
-#include <stdlib.h>
 
+#include "_net.h"
+#include "_pop.h"
 #include "_utils.h"
 
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define USAGE ("Usage: server port\nWhere port is < 65536")
+
+int8_t serve_peer(int udpsock) {
+        _pop_pkt_t *pop_packet;
+        uint8_t *udp_buffer;
+        ssize_t udpread;
+        size_t buffersize;
+        int8_t err;
+
+        buffersize = _UDP_MAX_DATA_PAYLOAD + 1;
+        udp_buffer = malloc(buffersize);
+        if (!udp_buffer)
+                exit(EXIT_FAILURE);
+
+        memset(udp_buffer, 0, buffersize);
+        err = _net_read_udp(udpsock, udp_buffer, buffersize, &udpread);
+        _check_err(err, "_net_read_udp: failed", _FATAL);
+
+        err = _pop_pkt_init(udp_buffer, udpread, &pop_packet);
+        _check_err(err, "_pop_pkt_init: reject not a pop packet", !_FATAL);
+        if (err) {
+                free(udp_buffer);
+                return 0;
+        }
+
+        err = _pop_pkt_destroy(&pop_packet);
+        _check_err(err, "_pop_pkt_destroy: failed", _FATAL);
+
+        free(udp_buffer);
+        return 0;
+}
+
+int8_t server_loop(int listener) {
+        size_t buffersize = _UDP_MAX_DATA_PAYLOAD + 1;
+        uint8_t *udp_buffer = malloc(buffersize);
+        if (!udp_buffer)
+                exit(EXIT_FAILURE);
+        memset(udp_buffer, 0, buffersize);
+
+        ssize_t udpread;
+        int8_t err;
+
+        for (;;) {
+                udpread = recv(listener, udp_buffer, buffersize, MSG_PEEK);
+                err = udpread < 0 ? -1 : 0;
+                _check_err(err, "recv: failed", _FATAL);
+                if (strcmp((const char *)udp_buffer, "q") == 0) {
+                        free(udp_buffer);
+                        exit(EXIT_SUCCESS);
+                }
+                err = serve_peer(listener);
+                _check_err(err, "server_peer: failed", _FATAL);
+        }
+
+        free(udp_buffer);
+        return 0;
+}
 
 int main(int argc, char **argv) {
         uintmax_t portnum;
         int8_t err;
+        int listener;
 
-        err = argc != 2;
-        _check_err(err, USAGE, true);
+        err = argc != 2 ? -1 : 0;
+        _check_err(err, USAGE, _FATAL);
 
         err = _parse_port(argv[1], &portnum);
-        _check_err(err, "err, _parse_port: failed", true);
+        _check_err(err, "err, _parse_port: failed", _FATAL);
 
-        err = portnum > 65535;
-        _check_err(err, "err, port out of range", true);
+        err = portnum > 65535 ? -1 : 0;
+        _check_err(err, "err, port out of range", _FATAL);
+
+        err = _net_listen_udp(argv[1], &listener);
+        _check_err(err, "_net_listen_udp: failed", _FATAL);
+
+        fprintf(stdout, "server listens on localhost:%s\n", argv[1]);
+        err = server_loop(listener);
+        _check_err(err, "server_loop: failed", _FATAL);
 
         return 0;
 }
